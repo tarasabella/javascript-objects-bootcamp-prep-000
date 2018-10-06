a0eb39a34621e335ab50f9a104cf9195df7b2069
Tara E. Sabella <tarasabella@gmail.com>
2018-10-06 21:27:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,11 +5,19 @@ function updatePlaylist(playlist, Bazzi, Beautiful) {
   playlist[Bazzi] = "Beautiful";
   return playlist;
 }
+<<<<<<< HEAD
 //creates a function that destructively adds an artist-song pair to the original
+=======
+//creates a function that destructively adds an artist-song pair to the original 
+>>>>>>> 3282fe95ca72ea2e0a39962f5dbc0ea2110c0736
 
 function removeFromPlaylist(playlist, Slowdive) {
   delete playlist.Slowdive;
 return playlist;
 }
 
+<<<<<<< HEAD
 //creates a function that destructively removes an artist from the playlist object
+=======
+//creates a function that destructively removes an artist from the playlist object
+>>>>>>> 3282fe95ca72ea2e0a39962f5dbc0ea2110c0736
